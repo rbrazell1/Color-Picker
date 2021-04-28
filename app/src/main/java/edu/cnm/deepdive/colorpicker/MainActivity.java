d252af507824c1b11aec9e227016109b5cb636f9
Russell Brazell <brazellrussell@gmail.com>
2021-04-28 09:14:42 -0600

    now show correct color code

--- app/src/main/java/edu/cnm/deepdive/colorpicker/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/colorpicker/MainActivity.java
@@ -13,12 +13,11 @@ public class MainActivity extends AppCompatActivity {
   private static final int MIN_S = 0;
   private static final int MAX_S = 255;
   private static final int PRESET_S = 150;
-  private static final int PRESET_COLOR = Color.YELLOW;
   private static final String RED_FORMAT = "Red: %d";
   private static final String GREEN_FORMAT = "Green: %d";
   private static final String BLUE_FORMAT = "Blue: %d";
   private static final String ALPHA_FORMAT = "Alpha: %d";
-  private static final String HEX_FORMAT = "Hex: # ";
+  private static final String HEX_FORMAT = "Hex: #%1$02x%2$02x%3$02x%4$02x";
   private int redValInt;
   private int greenValInt;
   private int blueValInt;
@@ -32,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
     setContentView(binding.getRoot());
-    binding.colorDisplay.setBackgroundColor(PRESET_COLOR);
     redSeekbar();
     greenSeekbar();
     blueSeekbar();
@@ -41,13 +39,11 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void findColor() {
-    int r = Color.red(redValInt);
-    int g = Color.green(greenValInt);
-    int b = Color.blue(blueValInt);
-    int a = Color.alpha(alphaValInt);
-    String hex = Integer.toHexString(a) + Integer.toHexString(r) + Integer.toHexString(g) + Integer.toHexString(b);
-    binding.displayValue.setText(String.format(HEX_FORMAT ,a + r + g + b));
-
+    int r = binding.redBar.getProgress();
+    int g = binding.greenBar.getProgress();
+    int b = binding.blueBar.getProgress();
+    int a = binding.alphaBar.getProgress();
+    binding.displayValue.setText(String.format(HEX_FORMAT , a , r , g , b));
   }
 
   private void redSeekbar() {
@@ -60,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         binding.redVal.setText(String.format(RED_FORMAT, progress));
         redValInt = progress;
         binding.colorDisplay.setBackgroundColor(Color.argb(alphaValInt, redValInt, greenValInt, blueValInt));
+        findColor();
       }
 
       @Override
@@ -83,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         binding.greenVal.setText(String.format(GREEN_FORMAT, progress));
         greenValInt = progress;
         binding.colorDisplay.setBackgroundColor(Color.argb(alphaValInt, redValInt, greenValInt, blueValInt));
+        findColor();
       }
 
       @Override
@@ -106,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         binding.blueVal.setText(String.format(BLUE_FORMAT, progress));
         blueValInt = progress;
         binding.colorDisplay.setBackgroundColor(Color.argb(alphaValInt, redValInt, greenValInt, blueValInt));
+        findColor();
       }
 
       @Override
@@ -129,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
         binding.alphaVal.setText(String.format(ALPHA_FORMAT, progress));
         alphaValInt = progress;
         binding.colorDisplay.setBackgroundColor(Color.argb(alphaValInt, redValInt, greenValInt, blueValInt));
+        findColor();
       }
 
       @Override
