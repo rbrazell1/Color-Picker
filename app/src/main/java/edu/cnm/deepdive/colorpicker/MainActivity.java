80509c545c96ea5b1424833b30a23487ea134133
Russell Brazell <brazellrussell@gmail.com>
2021-05-08 17:03:44 -0600

    added layout for landscape and new features. Need to remove app bar

--- app/src/main/java/edu/cnm/deepdive/colorpicker/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/colorpicker/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
+
     setContentView(binding.getRoot());
     redSeekbar();
     greenSeekbar();
